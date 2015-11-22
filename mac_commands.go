12542ebf7721f3f2b35550cad35fa57a44882de0
Orne Brocaar <info@brocaar.com>
2015-11-22 14:52:26 +0100

    Define MAC command identifiers.

---
+++ mac_commands.go
@@ -0,0 +1,25 @@
+package lorawan
+
+// CID defines the MAC command identifier.
+type CID byte
+
+// MAC commands as specified by the LoRaWAN R1.0 specs. Note that each *Req / *Ans
+// has the same value. Based on the fact if a message is uplink or downlink
+// you should use on or the other.
+const (
+	LinkCheckReq     CID = 0x02
+	LinkCheckAns     CID = 0x02
+	LinkADRReq       CID = 0x03
+	LinkADRAns       CID = 0x03
+	DutyCycleReq     CID = 0x04
+	DutyCycleAns     CID = 0x04
+	RXParamSetupReq  CID = 0x05
+	RXParamSetupAns  CID = 0x05
+	DevStatusReq     CID = 0x06
+	DevStatusAns     CID = 0x06
+	NewChannelReq    CID = 0x07
+	NewChannelAns    CID = 0x07
+	RXTimingSetupReq CID = 0x08
+	RXTimingSetupAns CID = 0x08
+	// 0x80 to 0xFF reserved for proprietary network command extensions
+)
