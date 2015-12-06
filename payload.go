0c9d588a6e1c143c2ddc6922beb05d8a3971c7f9
Orne Brocaar <info@brocaar.com>
2015-12-06 15:22:08 +0100

    Implement JoinAcceptPayload.

--- payload.go
+++ payload.go
@@ -66,3 +66,70 @@ func (p *JoinRequestPayload) UnmarshalBinary(data []byte) error {
 	p.DevNonce = binary.LittleEndian.Uint16(data[16:18])
 	return nil
 }
+
+// JoinAcceptPayload represents the join-accept message payload.
+// todo: implement CFlist
+type JoinAcceptPayload struct {
+	AppNonce   uint32 // only a value of up to 24 bits can be used
+	NetID      uint32 // only a value of up to 24 bits can be used
+	DevAddr    DevAddr
+	DLSettings DLsettings
+	RXDelay    uint8
+}
+
+// Clone returns a copy of the payload.
+func (p JoinAcceptPayload) Clone() Payload {
+	return &p
+}
+
+// MarshalBinary marshals the object in binary form.
+func (p JoinAcceptPayload) MarshalBinary() ([]byte, error) {
+	if p.AppNonce > 16777216 { // 2^24
+		return []byte{}, errors.New("lorawan: max value of AppNonce is 2^24 - 1")
+	}
+	if p.NetID > 16777216 {
+		return []byte{}, errors.New("lorawan: max value of NetID is 2^24 - 1")
+	}
+
+	var b []byte
+	var err error
+	out := make([]byte, 7, 12)
+	binary.LittleEndian.PutUint32(out[0:4], p.AppNonce)
+	binary.LittleEndian.PutUint32(out[3:7], uint32(p.NetID))
+	out = out[0:6] // drop the last empty byte (NetID is only 24 bit, so 8 MSB are 0)
+
+	b, err = p.DevAddr.MarshalBinary()
+	if err != nil {
+		return []byte{}, err
+	}
+	out = append(out, b...)
+
+	b, err = p.DLSettings.MarshalBinary()
+	if err != nil {
+		return []byte{}, err
+	}
+	out = append(out, b...)
+	out = append(out, byte(p.RXDelay))
+
+	return out, nil
+}
+
+// UnmarshalBinary decodes the object from binary form.
+func (p *JoinAcceptPayload) UnmarshalBinary(data []byte) error {
+	if len(data) != 12 {
+		return errors.New("lorawan: 12 bytes of data are expected")
+	}
+	i := make([]byte, 4) // Uint32 expects 4 bytes of data
+	copy(i, data[0:3])
+	p.AppNonce = binary.LittleEndian.Uint32(i)
+	copy(i, data[3:6])
+	p.NetID = binary.LittleEndian.Uint32(i)
+	if err := p.DevAddr.UnmarshalBinary(data[6:10]); err != nil {
+		return err
+	}
+	if err := p.DLSettings.UnmarshalBinary(data[10:11]); err != nil {
+		return err
+	}
+	p.RXDelay = uint8(data[11])
+	return nil
+}
--- payload_test.go
+++ payload_test.go
@@ -57,7 +57,7 @@ func TestJoinRequestPayload(t *testing.T) {
 			})
 		})
 
-		Convey("Given the slice []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}", func() {
+		Convey("Given a slice of bytes with an invalid size", func() {
 			b := make([]byte, 17)
 			Convey("Then UnmarshalBinary returns an error", func() {
 				err := p.UnmarshalBinary(b)
@@ -71,6 +71,54 @@ func TestJoinRequestPayload(t *testing.T) {
 				err := p.UnmarshalBinary(b)
 				So(err, ShouldBeNil)
 				So(p, ShouldResemble, JoinRequestPayload{AppEUI: 1, DevEUI: 2, DevNonce: 3})
+			})
+		})
+	})
+}
+
+func TestJoinAcceptPayload(t *testing.T) {
+	Convey("Given an empty JoinAcceptPayload", t, func() {
+		var p JoinAcceptPayload
+		Convey("Then MarshalBinary returns []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0", func() {
+			b, err := p.MarshalBinary()
+			So(err, ShouldBeNil)
+			So(b, ShouldResemble, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+		})
+
+		Convey("Given AppNonce=5, NetID=6, DevAddr=DevAddr(67305985), DLSettings=(RX2DataRate=7, RX1DRoffset=6), RXDelay=9", func() {
+			p.AppNonce = 5
+			p.NetID = 6
+			p.DevAddr = DevAddr(67305985)
+			p.DLSettings.RX2DataRate = 7
+			p.DLSettings.RX1DRoffset = 6
+			p.RXDelay = 9
+
+			Convey("Then MarshalBinary returns []byte{}", func() {
+				b, err := p.MarshalBinary()
+				So(err, ShouldBeNil)
+				So(b, ShouldResemble, []byte{5, 0, 0, 6, 0, 0, 1, 2, 3, 4, 103, 9})
+			})
+		})
+
+		Convey("Given a slice of bytes with an invalid size", func() {
+			b := make([]byte, 11)
+			Convey("Then UnmarshalBinary returns an error", func() {
+				err := p.UnmarshalBinary(b)
+				So(err, ShouldResemble, errors.New("lorawan: 12 bytes of data are expected"))
+			})
+		})
+
+		Convey("Given the slice []byte{5, 0, 0, 6, 0, 0, 1, 2, 3, 4, 103, 9}", func() {
+			b := []byte{5, 0, 0, 6, 0, 0, 1, 2, 3, 4, 103, 9}
+			Convey("Then UnmarshalBinary returns a JoinAcceptPayload with AppNonce=5, NetID=6, DevAddr=DevAddr(67305985), DLSettings=(RX2DataRate=7, RX1DRoffset=6), RXDelay=9", func() {
+				err := p.UnmarshalBinary(b)
+				So(err, ShouldBeNil)
+
+				So(p.AppNonce, ShouldEqual, 5)
+				So(p.NetID, ShouldEqual, 6)
+				So(p.DevAddr, ShouldEqual, DevAddr(67305985))
+				So(p.DLSettings, ShouldResemble, DLsettings{RX2DataRate: 7, RX1DRoffset: 6})
+				So(p.RXDelay, ShouldEqual, 9)
 			})
 		})
 	})
