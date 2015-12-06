e9104f3a3d2f65809862c9eeecba32c399ff2cd1
Orne Brocaar <info@brocaar.com>
2015-12-06 13:39:55 +0100

    Implement JoinRequestPayload.

--- payload.go
+++ payload.go
@@ -1,6 +1,10 @@
 package lorawan
 
-import "encoding"
+import (
+	"encoding"
+	"encoding/binary"
+	"errors"
+)
 
 // Payload is the interface that every payload needs to implement.
 type Payload interface {
@@ -28,5 +32,37 @@ func (p DataPayload) MarshalBinary() ([]byte, error) {
 func (p *DataPayload) UnmarshalBinary(data []byte) error {
 	p.Bytes = make([]byte, len(data))
 	copy(p.Bytes, data)
+	return nil
+}
+
+// JoinRequestPayload represents the join-request message payload.
+type JoinRequestPayload struct {
+	AppEUI   uint64
+	DevEUI   uint64
+	DevNonce uint16
+}
+
+// Clone returns a copy of the payload.
+func (p JoinRequestPayload) Clone() Payload {
+	return &p
+}
+
+// MarshalBinary marshals the object in binary form.
+func (p JoinRequestPayload) MarshalBinary() ([]byte, error) {
+	b := make([]byte, 18)
+	binary.LittleEndian.PutUint64(b[0:8], p.AppEUI)
+	binary.LittleEndian.PutUint64(b[8:16], p.DevEUI)
+	binary.LittleEndian.PutUint16(b[16:18], p.DevNonce)
+	return b, nil
+}
+
+// UnmarshalBinary decodes the object from binary form.
+func (p *JoinRequestPayload) UnmarshalBinary(data []byte) error {
+	if len(data) != 18 {
+		return errors.New("lorawan: 18 bytes of data are expected")
+	}
+	p.AppEUI = binary.LittleEndian.Uint64(data[0:8])
+	p.DevEUI = binary.LittleEndian.Uint64(data[8:16])
+	p.DevNonce = binary.LittleEndian.Uint16(data[16:18])
 	return nil
 }
--- payload_test.go
+++ payload_test.go
@@ -1,6 +1,7 @@
 package lorawan
 
 import (
+	"errors"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -31,6 +32,45 @@ func TestDataPayload(t *testing.T) {
 				So(err, ShouldBeNil)
 				So(p.Bytes, ShouldNotEqual, b) // make sure we get a new copy!
 				So(p.Bytes, ShouldResemble, b)
+			})
+		})
+	})
+}
+
+func TestJoinRequestPayload(t *testing.T) {
+	Convey("Given an empty JoinRequestPayload", t, func() {
+		var p JoinRequestPayload
+		Convey("Then MarshalBinary returns []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}", func() {
+			b, err := p.MarshalBinary()
+			So(err, ShouldBeNil)
+			So(b, ShouldResemble, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+		})
+
+		Convey("Given AppEUI=1, DevEUI=2, DevNonce=3", func() {
+			p.AppEUI = 1
+			p.DevEUI = 2
+			p.DevNonce = 3
+			Convey("Then MarshalBinary returns []byte{1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0}", func() {
+				b, err := p.MarshalBinary()
+				So(err, ShouldBeNil)
+				So(b, ShouldResemble, []byte{1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0})
+			})
+		})
+
+		Convey("Given the slice []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}", func() {
+			b := make([]byte, 17)
+			Convey("Then UnmarshalBinary returns an error", func() {
+				err := p.UnmarshalBinary(b)
+				So(err, ShouldResemble, errors.New("lorawan: 18 bytes of data are expected"))
+			})
+		})
+
+		Convey("Given the slice []byte{1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0}", func() {
+			b := []byte{1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0}
+			Convey("Then UnmarshalBinary returns a JoinRequestPayload with AppEUI=1, DevEUI=2, DevNonce=3", func() {
+				err := p.UnmarshalBinary(b)
+				So(err, ShouldBeNil)
+				So(p, ShouldResemble, JoinRequestPayload{AppEUI: 1, DevEUI: 2, DevNonce: 3})
 			})
 		})
 	})
