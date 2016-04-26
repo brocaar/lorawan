37bb1f35e6d571a68a6907b4db7fa402c5bc687e
Mehrad <mehrad.arashrad@gmail.com>
2016-04-26 17:29:16 +1000

    The AU and US band had some issues with the rx timestamp which is fixed and tested (Test only has been done on AU_915)

--- band/band_au915_928.go
+++ band/band_au915_928.go
@@ -157,13 +157,13 @@ func getChannelNumber(frequency, dataRate int) (int, error) {
 
 // Default settings for this band
 const (
-	ReceiveDelay1    time.Duration = 1
-	ReceiveDelay2    time.Duration = 2
-	JoinAcceptDelay1 time.Duration = 5
-	JoinAcceptDelay2 time.Duration = 6
+	ReceiveDelay1    time.Duration = time.Second
+	ReceiveDelay2    time.Duration = time.Second * 2
+	JoinAcceptDelay1 time.Duration = time.Second * 5
+	JoinAcceptDelay2 time.Duration = time.Second * 6
 	MaxFCntGap       uint32        = 16384
 	ADRAckLimit                    = 64
 	ADRAckDelay                    = 32
-	AckTimeoutMin    time.Duration = 1
-	AckTimeoutMax    time.Duration = 3
+	AckTimeoutMin    time.Duration = time.Second // AckTimeout = 2 +/- 1 (random value between 1 - 3)
+	AckTimeoutMax    time.Duration = time.Second * 3
 )
